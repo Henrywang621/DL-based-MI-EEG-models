effd19ca1f217fdc28c54f980a256d24702749e3
Henrywang <49090477+Henrywang621@users.noreply.github.com>
2025-04-22 20:46:04 +0800

    Update train.sh

--- BCIIV2a_CrossSubjs/train.sh
+++ BCIIV2a_CrossSubjs/train.sh
@@ -2,23 +2,23 @@
 eval "$(conda shell.bash hook)"
 
 conda activate tf-gpu
-nohup python train_EEGNet_4_2.py --numepochs 300 --Patience4ES 30
-nohup python train_EEGNet_8_2.py --numepochs 300 --Patience4ES 30
-nohup python train_ETENet.py --numepochs 300 --Patience4ES 30  
-nohup python train_EEGfusion.py --numepochs 300 --Patience4ES 30
-nohup python train_Min2Net.py --numepochs 300 --Patience4ES 30
-nohup python train_pCNN.py --numepochs 300 --Patience4ES 30
-nohup python train_LSTM.py --numepochs 300 --Patience4ES 30
+nohup python train_EEGNet_4_2.py --numepochs 500 --Patience4ES 30
+nohup python train_EEGNet_8_2.py --numepochs 500 --Patience4ES 30
+nohup python train_ETENet.py --numepochs 500 --Patience4ES 30  
+nohup python train_EEGfusion.py --numepochs 500 --Patience4ES 30
+nohup python train_Min2Net.py --numepochs 500 --Patience4ES 30
+nohup python train_pCNN.py --numepochs 500 --Patience4ES 30
+nohup python train_LSTM.py --numepochs 500 --Patience4ES 30
 
 
 
 conda activate torch37c
-nohup python train_CLSTM.py --numepochs 300 --Patience4ES 30
+nohup python train_CLSTM.py --numepochs 500 --Patience4ES 30
 conda activate torch37b6
-nohup python train_ShallowConvNet.py --numepochs 300 --Patience4ES 30
-nohup python train_DeepConvNet.py --numepochs 300 --Patience4ES 30
+nohup python train_ShallowConvNet.py --numepochs 500 --Patience4ES 30
+nohup python train_DeepConvNet.py --numepochs 500 --Patience4ES 30
 conda activate torch37c
-nohup python -u train_SEFFNet.py --numepochs 300 --Patience4ES 30
+nohup python -u train_SEFFNet.py --numepochs 500 --Patience4ES 30
 
 
 
